272e4d514c8a3b7bf0850ee8c73b21515c5e1aae
sherouk22 <sheroukmohy558@gmail.com>
2025-01-22 03:33:44 +0200

    Assignment07C#OOP04

--- Assignment08C#OOP05/Program.cs
+++ Assignment08C#OOP05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment08C_OOP05.First_Project;
+using Assignment08C_OOP05.Second_Project;
 
 namespace Assignment08C_OOP05
 {
@@ -11,71 +12,88 @@ namespace Assignment08C_OOP05
 
 
 
-           // _3DPoint P = new _3DPoint(10, 10, 10);
-           // Console.WriteLine(P.ToString());
-           // _3DPoint P1 = new _3DPoint();
-           // Console.WriteLine("Enter  Point P1:");
-           // P1.X = int.Parse(Console.ReadLine());
-           // P1.Y = int.Parse(Console.ReadLine());
-           // P1.Z = int.Parse(Console.ReadLine());
-           // Console.WriteLine(P1.ToString());
+            // _3DPoint P = new _3DPoint(10, 10, 10);
+            // Console.WriteLine(P.ToString());
+            // _3DPoint P1 = new _3DPoint();
+            // Console.WriteLine("Enter  Point P1:");
+            // P1.X = int.Parse(Console.ReadLine());
+            // P1.Y = int.Parse(Console.ReadLine());
+            // P1.Z = int.Parse(Console.ReadLine());
+            // Console.WriteLine(P1.ToString());
 
-           // Console.WriteLine("Enter  Point P2:");
-           // _3DPoint P2 = new _3DPoint();
-           // P2.X = int.Parse(Console.ReadLine());
-           // P2.Y = int.Parse(Console.ReadLine());
-           // P2.Z = int.Parse(Console.ReadLine());
-           // Console.WriteLine(P2.ToString());
+            // Console.WriteLine("Enter  Point P2:");
+            // _3DPoint P2 = new _3DPoint();
+            // P2.X = int.Parse(Console.ReadLine());
+            // P2.Y = int.Parse(Console.ReadLine());
+            // P2.Z = int.Parse(Console.ReadLine());
+            // Console.WriteLine(P2.ToString());
 
-           // Console.WriteLine("Enter  Point P3:");
-           // _3DPoint P3 = new _3DPoint();
-           // P2.X = int.Parse(Console.ReadLine());
-           // P2.Y = int.Parse(Console.ReadLine());
-           // P2.Z = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Enter  Point P3:");
+            // _3DPoint P3 = new _3DPoint();
+            // P2.X = int.Parse(Console.ReadLine());
+            // P2.Y = int.Parse(Console.ReadLine());
+            // P2.Z = int.Parse(Console.ReadLine());
 
-           // Console.WriteLine(P3.ToString());
+            // Console.WriteLine(P3.ToString());
 
-           // if (P1 == P2)
-           // {
-           //     Console.WriteLine("P1 and P2 are equal.");
-           // }
-           // else
-           // {
-           //     Console.WriteLine("P1 and P2 are not equal");
-           // }
-
-           
-
-           // _3DPoint[] points = new _3DPoint[] { P1, P2 ,P3};
-
-           // foreach (var point in points)
-           // {
-           //     Console.WriteLine(point);
-           // }
-
-           // Array.Sort(points);
-
-           // Console.WriteLine("Sorted Array:");
-           // foreach (var point in points)
-           // {
-           //     Console.WriteLine(point);
-           // }
+            // if (P1 == P2)
+            // {
+            //     Console.WriteLine("P1 and P2 are equal.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("P1 and P2 are not equal");
+            // }
 
 
-           //_3DPoint P4 = new _3DPoint(5, 6, 7);
-           //_3DPoint P5 = new _3DPoint(1, 2, 3);
+
+            // _3DPoint[] points = new _3DPoint[] { P1, P2 ,P3};
+
+            // foreach (var point in points)
+            // {
+            //     Console.WriteLine(point);
+            // }
+
+            // Array.Sort(points);
+
+            // Console.WriteLine("Sorted Array:");
+            // foreach (var point in points)
+            // {
+            //     Console.WriteLine(point);
+            // }
 
 
-           // Console.WriteLine($" Point 1: {P4.ToString()}");
-           // Console.WriteLine($" Point 2: {P5.ToString()}");
+            //_3DPoint P4 = new _3DPoint(5, 6, 7);
+            //_3DPoint P5 = new _3DPoint(1, 2, 3);
 
-           // P5 = (_3DPoint)P4.Clone();
-           // P4.X = 15;
-           // Console.WriteLine($" Point 1: {P4}");
-           // Console.WriteLine($"Point2: {P5}");
+
+            // Console.WriteLine($" Point 1: {P4.ToString()}");
+            // Console.WriteLine($" Point 2: {P5.ToString()}");
+
+            // P5 = (_3DPoint)P4.Clone();
+            // P4.X = 15;
+            // Console.WriteLine($" Point 1: {P4}");
+            // Console.WriteLine($"Point2: {P5}");
 
 
             #endregion
+
+            #region Second Project:
+
+            //Console.WriteLine("Enter the first number:");
+            //double A = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the second number:");
+            //double B = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Addition: {Maths.Add(A, B)}");
+            //Console.WriteLine($"Subtraction: {Maths.Subtract(A, B)}");
+            //Console.WriteLine($"Multiplication: {Maths.Multiply(A, B)}");
+            //Console.WriteLine($"Division: {Maths.Divide(A,B)}");
+
+
+            #endregion
+
 
         }
     }
---
+++ Assignment08C#OOP05/Second Project/Maths.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment08C_OOP05.Second_Project
+{
+    public  class Maths
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+
+    }
+}
