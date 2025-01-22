5181cd892c8fdb65d545ce5b42b97b75b5fe2962
sherouk22 <sheroukmohy558@gmail.com>
2025-01-22 03:18:34 +0200

    Assignment07C#OOP04

---
+++ Assignment08C#OOP05/First Project/3DPoint.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment08C_OOP05.First_Project
+{
+    public  class _3DPoint : IComparable , ICloneable
+    {
+      
+
+        public int X {  get; set; }
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+
+        public _3DPoint() { 
+        }
+        public _3DPoint(int x, int y) : this (x ,y ,0)
+        {
+        
+        }
+
+        public _3DPoint(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+
+        public int CompareTo(object? obj)
+        {
+            _3DPoint point3D = (_3DPoint)obj;
+
+            if(X > point3D.X && Y> point3D.Y)
+                return 1;
+            else if(X < point3D.X && Y < point3D.Y)
+                return -1;
+            else return 0;
+
+        }
+
+        public object Clone()
+        {
+            return new _3DPoint(X, Y, Z);
+        }
+    }
+
+
+}
--- Assignment08C#OOP05/Program.cs
+++ Assignment08C#OOP05/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace Assignment08C_OOP05
+﻿using Assignment08C_OOP05.First_Project;
+
+namespace Assignment08C_OOP05
 {
     internal class Program
     {//Assignment07C#OOP04
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region  First Project:
+
+
+
+           // _3DPoint P = new _3DPoint(10, 10, 10);
+           // Console.WriteLine(P.ToString());
+           // _3DPoint P1 = new _3DPoint();
+           // Console.WriteLine("Enter  Point P1:");
+           // P1.X = int.Parse(Console.ReadLine());
+           // P1.Y = int.Parse(Console.ReadLine());
+           // P1.Z = int.Parse(Console.ReadLine());
+           // Console.WriteLine(P1.ToString());
+
+           // Console.WriteLine("Enter  Point P2:");
+           // _3DPoint P2 = new _3DPoint();
+           // P2.X = int.Parse(Console.ReadLine());
+           // P2.Y = int.Parse(Console.ReadLine());
+           // P2.Z = int.Parse(Console.ReadLine());
+           // Console.WriteLine(P2.ToString());
+
+           // Console.WriteLine("Enter  Point P3:");
+           // _3DPoint P3 = new _3DPoint();
+           // P2.X = int.Parse(Console.ReadLine());
+           // P2.Y = int.Parse(Console.ReadLine());
+           // P2.Z = int.Parse(Console.ReadLine());
+
+           // Console.WriteLine(P3.ToString());
+
+           // if (P1 == P2)
+           // {
+           //     Console.WriteLine("P1 and P2 are equal.");
+           // }
+           // else
+           // {
+           //     Console.WriteLine("P1 and P2 are not equal");
+           // }
+
+           
+
+           // _3DPoint[] points = new _3DPoint[] { P1, P2 ,P3};
+
+           // foreach (var point in points)
+           // {
+           //     Console.WriteLine(point);
+           // }
+
+           // Array.Sort(points);
+
+           // Console.WriteLine("Sorted Array:");
+           // foreach (var point in points)
+           // {
+           //     Console.WriteLine(point);
+           // }
+
+
+           //_3DPoint P4 = new _3DPoint(5, 6, 7);
+           //_3DPoint P5 = new _3DPoint(1, 2, 3);
+
+
+           // Console.WriteLine($" Point 1: {P4.ToString()}");
+           // Console.WriteLine($" Point 2: {P5.ToString()}");
+
+           // P5 = (_3DPoint)P4.Clone();
+           // P4.X = 15;
+           // Console.WriteLine($" Point 1: {P4}");
+           // Console.WriteLine($"Point2: {P5}");
+
+
+            #endregion
+
         }
     }
 }
